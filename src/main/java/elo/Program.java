1f7d9560bd222e5e9db445769982754ee799fdaa
Farhan Faruqui <farhanfaruqui13@gmail.com>
2021-01-03 15:07:32 -0500

    Fixing project structure getCookies completed

---
+++ src/main/java/elo/Program.java
@@ -0,0 +1,10 @@
+package elo;
+
+import kong.unirest.Cookies;
+
+public class Program {
+    public static  void main(String[] args) {
+        Cookies cookies = Authentication.getCookies();
+
+    }
+}
