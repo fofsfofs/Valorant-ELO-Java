d5e8f9440ac4fedc0c4cd0cb9f870f1b4351c211
Farhan Faruqui <farhanfaruqui13@gmail.com>
2021-01-07 16:20:51 -0500

    Changed version string to 1.2

--- src/main/java/elo/Program.java
+++ src/main/java/elo/Program.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Program extends Application {
-    public final static String version = "1.14";
+    final static String version = "1.2";
 
     public static void main(String[] args) {
         launch(args);
