63c56ef796f528801f9cb74971b6d2e162163727
Farhan Faruqui <farhanfaruqui13@gmail.com>
2021-01-07 13:44:02 -0500

    Updates now work with jars AND exes

--- src/main/java/elo/Updater.java
+++ src/main/java/elo/Updater.java
@@ -11,23 +11,31 @@ import javafx.scene.control.ButtonType;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Updater {
     DbxRequestConfig config = DbxRequestConfig.newBuilder("").build();
     DbxClientV2 client = new DbxClientV2(config, Secret.dbToken);
     private Login login;
+    private String extension = "";
 
     public Updater(Login l) {
         this.login = l;
-        deleteOld();
-        updateNeeded();
+        getExtension();
+        if (!extension.equals("dev")) {
+            deleteOld();
+            updateNeeded();
+        } else {
+            login.createLogin();
+        }
     }
 
     private void deleteOld() {
         String[] paths;
         paths = new File(System.getProperty("user.dir")).list();
         for (String path : paths) {
-            if (path.contains(".jar") && !path.equals(Program.version + ".jar")) {
+            if (path.contains(extension) && !path.contains(Program.version + "." + extension)) {
                 File file = new File(path);
                 file.delete();
             }
@@ -37,16 +45,16 @@ public class Updater {
     private String getLatestVersionName() {
         ListFolderResult result = null;
         try {
-            result = client.files().listFolder("");
+            result = client.files().listFolder("/" + extension.toUpperCase() + "s");
         } catch (DbxException e) {
             e.printStackTrace();
         }
         String file = result.getEntries().get(0).getPathDisplay();
-        return file.substring(1);
+        return file.substring(6);
     }
 
     private double getLatestVersionNum() {
-        return Double.parseDouble(getLatestVersionName().substring(13, getLatestVersionName().indexOf(".jar")));
+        return Double.parseDouble(getLatestVersionName().substring(13, getLatestVersionName().indexOf("." + extension)));
     }
 
     private void updateNeeded() {
@@ -62,7 +70,11 @@ public class Updater {
                 if (type == yes) {
                     update();
                     try {
-                        Process proc = Runtime.getRuntime().exec("java -jar " + getLatestVersionName());
+                        if (extension.equals("jar")) {
+                            Process proc = Runtime.getRuntime().exec("java -jar " + getLatestVersionName());
+                        } else if (extension.equals("exe")) {
+                            Process proc = Runtime.getRuntime().exec(getLatestVersionName());
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -75,11 +87,25 @@ public class Updater {
         }
     }
 
+    private void getExtension() {
+        String[] paths;
+        paths = new File(System.getProperty("user.dir")).list();
+        List<String> l = Arrays.asList(paths);
+        if (l.contains(String.format("Valorant-ELO-%s.jar", Program.version))) {
+            extension = "jar";
+        } else if (l.contains(String.format("Valorant-ELO-%s.exe", Program.version))) {
+            extension = "exe";
+        } else {
+            extension = "dev";
+        }
+    }
+
     private void update() {
         try {
             File file = new File(getLatestVersionName());
             FileOutputStream fOut = new FileOutputStream(file);
-            client.files().downloadBuilder("/" + getLatestVersionName()).start().download(fOut);
+            client.files().downloadBuilder("/" + extension.toUpperCase() + "s/" + getLatestVersionName()).start().download(fOut);
+            fOut.close();
         } catch (DbxException e) {
             e.printStackTrace();
         } catch (IOException e) {
