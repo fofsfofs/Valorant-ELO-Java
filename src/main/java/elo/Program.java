439a018e76abf9853ce2b6a6e71d9f8cbf93908c
Farhan Faruqui <farhanfaruqui13@gmail.com>
2021-03-02 17:46:35 -0500

    2.34, Astra update

--- src/main/java/elo/Program.java
+++ src/main/java/elo/Program.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Program extends Application {
-    final static String version = "2.33";
+    final static String version = "2.34";
     static final Logger logger = LogManager.getLogger(Program.class);
 
     public static void main(String[] args) {
